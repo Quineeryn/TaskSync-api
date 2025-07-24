50e92cce08f396400e94d10e1d86b97dd8977628
Quineeryn <canandraeka10@gmail.com>
2025-07-24 20:35:56 +0700

    Initial commit: Setup Spring Boot project structure

---
+++ src/main/java/com/portfolio/tasksync_api/TasksyncApiApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.tasksync_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksyncApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksyncApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/tasksync_api/TasksyncApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.tasksync_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksyncApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
